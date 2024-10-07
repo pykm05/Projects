feef87b9d6edff5d2e13390302f796c8736e5d31
Aaron Pang <pykm05@gmail.com>
2024-10-07 00:04:07 -0700

    added defer to js link and created basic structure for book object

--- JS/Library/script.js
+++ JS/Library/script.js
@@ -1,9 +1,21 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+// Book constructor
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(Book) {
   // do stuff here
+  myLibrary.push(Book);
+  let newBook = document.createElement("div");
+  newBook.classList.add("book");
+  newBook.textContent = Book.author;
+  let container = document.getElementById("book-container");
+  container.appendChild(newBook);
 }
+
+addBookToLibrary(new Book("aaron", "skibidi", 123, true));
