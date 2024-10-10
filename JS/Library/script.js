8bfdeec0625ed05c2bb12513c499cca1852fd237
Aaron Pang <pykm05@gmail.com>
2024-10-09 22:34:48 -0700

    receive form input in JS and append title and author to book list

--- JS/Library/script.js
+++ JS/Library/script.js
@@ -9,13 +9,23 @@ function Book(author, title, pages, read) {
 }
 
 function addBookToLibrary(Book) {
-  // do stuff here
   myLibrary.push(Book);
-  let newBook = document.createElement("div");
-  newBook.classList.add("book");
-  newBook.textContent = Book.author;
-  let container = document.getElementById("book-container");
-  container.appendChild(newBook);
 }
 
-addBookToLibrary(new Book("aaron", "skibidi", 123, true));
+function createBook(event) {
+  const author = document.querySelector("#author");
+  const title = document.querySelector("#title");
+  const pages = document.querySelector("#pageCount");
+  const read = document.querySelector("#hasRead");
+
+  let newBook = document.createElement("div");
+  newBook.classList.add("book");
+  newBook.textContent = title.value + " --------- By: " + author.value;
+  let container = document.getElementById("book-container");
+  container.appendChild(newBook);
+
+  event.preventDefault();
+}
+
+const submit = document.querySelector("#submit");
+submit.addEventListener('click', createBook, false);
